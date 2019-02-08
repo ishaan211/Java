5899d397e11eddb24212ba5aa68df885b747d2d6
ishaan211 <41157452+ishaan211@users.noreply.github.com>
2019-02-08 23:14:41 +0530

    Add files via upload

---
+++ Find.java
@@ -0,0 +1,35 @@
+
+public class Find {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "hey";
+		String str2 = "maven";
+		String str3 = "tez";
+		String str4 = "miniaturey";
+		String str5 = "ray";
+		String str6 = "rule";
+		int count =0;
+		 if(str1.endsWith("y")||str1.endsWith("z")) {
+			count++;
+		 }
+		 if(str2.endsWith("y")||str2.endsWith("z")) {
+				count++;
+			 }
+		 if(str3.endsWith("y")||str3.endsWith("z")) {
+				count++;
+			 }
+		 if(str4.endsWith("y")||str4.endsWith("z")) {
+				count++;
+			 }
+		 if(str5.endsWith("y")||str5.endsWith("z")) {
+				count++;
+			 }
+		 if(str6.endsWith("y")||str6.endsWith("z")) {
+				count++;
+			 }
+		 System.out.println("total: "+count);
+	}
+	
+
+}

68d5d725902a3b19ca1d2294a48a8b224bad612b
ishaan211 <41157452+ishaan211@users.noreply.github.com>
2019-02-22 17:52:34 +0530

    Update FindWithMethod.java

--- FindWithMethod.java
+++ FindWithMethod.java
@@ -1,13 +1,13 @@
 
 public class FindWithMethod {
 	String str ="hello my name is faraz and I am a silly boy and I want to go to suez";
-	int count=1;
-	int county=1;
-	int countz=1;
+	int count=0;
+	int county=0;
+	int countz=0;
 	void getTotalWordCount(){
 		for(int i=0;i < str.length();i++){
 			char ch = str.charAt(i);
-			if(str.charAt(i)==' '){
+			if(str.charAt(i)==' ' && str.charAt(i)!==' '{
 				count++;
 			}
 			
